917f894c545518e9bab5aed3b3f54937f536fa30
Matthew Leon <ml@matthewleon.com>
2017-03-15 14:01:59 +0000

    faster random int test
    avoid overhead of unfoldr

--- README.md
+++ README.md
@@ -2,8 +2,6 @@ WIP
 
 TODO: move list dep to devdeps (seed using only STArray)
 
-TODO: tests
-
-TODO: simple benchmarking
+TODO: tests (compare to Python wikipedia source)
 
 TODO: think about API
--- test/Main.purs
+++ test/Main.purs
@@ -2,23 +2,31 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Control.Monad.Eff.Now (NOW, now)
-import Data.Array (fromFoldable)
 import Data.DateTime.Instant (unInstant)
-import Data.List.Lazy (take)
-import Data.List.Lazy.Types (List)
-import Data.Maybe (Maybe(..))
-import Data.Unfoldable (unfoldr)
+import Data.List (length, reverse)
+import Data.List.Types (List(..), (:))
+import Data.Tuple (Tuple(..))
 
 import System.Random.Mersenne (seed, int32)
+import System.Random.Mersenne.Types (MTState)
 
 main :: Eff (now :: NOW, console :: CONSOLE) Unit
 main = do
-  start <- unInstant <$> now
-  let ints = fromFoldable $ take 100000 (lazyRandomInts 100)
-  generated <- unInstant <$> now
-  logShow $ generated - start
+  start <- now
+  let ints = randomInts 100 1000000
+  generated <- now
+  log $ "generated list of " <> show (length ints) <> " random ints."
+  logShow (unInstant generated - unInstant start)
   where
-    lazyRandomInts :: Int -> List Int
-    lazyRandomInts i = unfoldr (Just <<< int32) (seed i)
+    randomInts :: Int -> Int -> List Int
+    randomInts s length = reverse $ go (seed s) Nil 0
+      where
+        go :: MTState -> List Int -> Int -> List Int
+        -- tail recursive, builds backward
+        go state accum index
+          | (index < length) =
+            case int32 state
+              of (Tuple i state') -> go state' (i : accum) (index + 1)
+          | otherwise = accum
